2cbb52d167725069339bb6b79624ba5b31207d3d
Edward Amor <edward.amor3@gmail.com>
2021-08-06 03:47:50 -0400

    test: update ve_delegation fixture

--- tests/fixtures/deployments.py
+++ tests/fixtures/deployments.py
@@ -1,6 +1,9 @@
 import pytest
+from brownie import ZERO_ADDRESS
 
 
 @pytest.fixture(scope="session")
 def ve_delegation(alice, VotingEscrowDelegation):
-    return VotingEscrowDelegation.deploy({"from": alice})
+    return VotingEscrowDelegation.deploy(
+        "Curve veCRV Boost", "veCRV-Boost", ZERO_ADDRESS, {"from": alice}
+    )

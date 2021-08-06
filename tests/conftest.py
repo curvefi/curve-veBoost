a9027c401dba3410fd45f1b832577edc9840dc70
Edward Amor <edward.amor3@gmail.com>
2021-08-05 21:31:50 -0400

    test: add custom module/function level isolation fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,20 @@
+import pytest
+
 pytest_plugins = ["fixtures.accounts"]
+
+
+@pytest.fixture(scope="module")
+def module_isolation(chain):
+    """Custom module level isolation, preserves session level fixtures."""
+    chain.snapshot()
+    yield
+    chain.revert()
+
+
+@pytest.fixture(autouse=True)
+def function_isolation(chain, history, module_isolation):
+    """Custom function level isolation."""
+    start = len(history)
+    yield
+    if (undo_count := len(history) - start) > 0:
+        chain.undo(undo_count)

07134fdee2221f31a0e8ba6762e5ca9653455db3
Edward Amor <edward.amor3@gmail.com>
2021-08-09 03:31:31 -0400

    test: update fixtures to account for local crv + vecrv deployments

--- tests/fixtures/deployments.py
+++ tests/fixtures/deployments.py
@@ -1,6 +1,23 @@
 import pytest
+from brownie import ZERO_ADDRESS, compile_source
 
 
 @pytest.fixture(scope="session")
-def ve_delegation(alice, VotingEscrowDelegation):
-    return VotingEscrowDelegation.deploy("Curve veCRV Boost", "veCRV-Boost", {"from": alice})
+def crv(alice, pm):
+    ERC20CRV = pm("curvefi/curve-dao-contracts@1.1.0").ERC20CRV
+    return ERC20CRV.deploy("Curve DAO Token", "CRV", 18, {"from": alice})
+
+
+@pytest.fixture(scope="session")
+def vecrv(alice, crv, pm):
+    VotingEscrow = pm("curvefi/curve-dao-contracts@1.1.0").VotingEscrow
+    return VotingEscrow.deploy(crv, "Vote-Escrowed CRV", "veCRV", "Version 1.0.0", {"from": alice})
+
+
+@pytest.fixture(scope="session")
+def ve_delegation(alice, VotingEscrowDelegation, vecrv):
+    source = VotingEscrowDelegation._build["source"]
+    source = source.replace(ZERO_ADDRESS, vecrv.address)
+
+    NewVotingEscrowDelegation = compile_source(source).Vyper
+    return NewVotingEscrowDelegation.deploy("Curve veCRV Boost", "veCRV-Boost", {"from": alice})

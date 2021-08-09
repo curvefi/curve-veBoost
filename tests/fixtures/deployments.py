49cade646f18e710a814070d25018f84203d1f2c
Edward Amor <edward.amor3@gmail.com>
2021-08-09 03:11:11 -0400

    fix(test): voting escrow constructor args

--- tests/fixtures/deployments.py
+++ tests/fixtures/deployments.py
@@ -1,9 +1,6 @@
 import pytest
-from brownie import ZERO_ADDRESS
 
 
 @pytest.fixture(scope="session")
 def ve_delegation(alice, VotingEscrowDelegation):
-    return VotingEscrowDelegation.deploy(
-        "Curve veCRV Boost", "veCRV-Boost", ZERO_ADDRESS, {"from": alice}
-    )
+    return VotingEscrowDelegation.deploy("Curve veCRV Boost", "veCRV-Boost", {"from": alice})

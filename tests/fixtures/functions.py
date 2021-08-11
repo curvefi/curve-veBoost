3a7f32fbaff64f645f7e12ae86043abbe43b734b
Edward Amor <edward.amor3@gmail.com>
2021-08-11 02:46:56 -0400

    fix(test): remove token_id fixture

--- tests/fixtures/functions.py
+++ tests/fixtures/functions.py
@@ -1,5 +1,4 @@
 import pytest
-from brownie import convert
 
 
 @pytest.fixture
@@ -13,11 +12,6 @@ def lock_bob(alice, bob, bob_lock_value, bob_unlock_time, crv, vecrv):
     crv.transfer(bob, bob_lock_value, {"from": alice})
     crv.approve(vecrv, bob_lock_value, {"from": bob})
     vecrv.create_lock(bob_lock_value, bob_unlock_time, {"from": bob})
-
-
-@pytest.fixture(scope="session")
-def token_id(alice):
-    return lambda account, _id: _id + convert.to_uint(account) << 96
 
 
 @pytest.fixture

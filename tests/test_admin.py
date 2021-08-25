650276fbaed572c1a1898945cdc3d8d8d7f808a1
Edward Amor <edward.amor3@gmail.com>
2021-08-24 20:07:24 -0400

    fix: remove kill switch in veboost

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,5 +1,4 @@
 import brownie
-import pytest
 
 
 def test_commit_new_owner(alice, bob, veboost):
@@ -23,25 +22,6 @@ def test_accept_transfer_ownership_guarded(alice, bob, veboost):
     veboost.commit_transfer_ownership(bob, {"from": alice})
     with brownie.reverts():
         veboost.accept_transfer_ownership({"from": alice})
-
-
-@pytest.mark.usefixtures("boost_bob")
-def test_set_killed(alice, bob, veboost):
-    assert veboost.adjusted_balance_of(bob) > 0
-
-    veboost.set_killed(True, {"from": alice})
-    assert veboost.adjusted_balance_of(bob) == 0
-
-    veboost.set_killed(False, {"from": alice})
-    assert veboost.adjusted_balance_of(bob) > 0
-
-
-def test_set_killed_guarded(bob, veboost):
-    with brownie.reverts():
-        veboost.set_killed(True, {"from": bob})
-
-    with brownie.reverts():
-        veboost.set_killed(False, {"from": bob})
 
 
 def test_set_base_uri(alice, veboost):

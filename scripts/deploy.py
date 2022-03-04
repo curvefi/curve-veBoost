a282d0814b98f5d940d93289d20dcadfc90f6c00
Edward Amor <edward.amor3@gmail.com>
2022-03-04 23:09:02 +0000

    fix: deploy script to also deploy ve wrapper

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,19 +1,14 @@
-from brownie import VotingEscrowDelegation, accounts
+from brownie import VotingEscrowDelegation, VotingEscrowWrapper, accounts
 
 
 def main():
     DEPLOYER = accounts.load("veOracle-deployer")
+
     VotingEscrowDelegation.deploy(
         "Voting Escrow Boost Delegation",
         "veBoost",
         "",
         {"from": DEPLOYER},
     )
-    with open("etherscan_source_verification.vy", "w") as f:
-        # we use jinja in the file, but the deployed source will
-        # not have any testing code blocks
-        # can verify by trying in mainnet-fork
-        # brownie run deploy --network mainnet-fork -I
-        # and checking the sauce
-        sauce = VotingEscrowDelegation._build["source"]
-        f.write(sauce)
+    VotingEscrowWrapper.deploy({"from": DEPLOYER})
+

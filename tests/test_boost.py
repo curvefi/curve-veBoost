02e2304780eaee27b00aeea9f98bd6eebb437d91
wavey0x <retro.waveyy@gmail.com>
2024-07-07 07:32:12 -0400

    test: add test for adjusted_balance_of_write

--- tests/test_boost.py
+++ tests/test_boost.py
@@ -1,8 +1,11 @@
 import ape
+from ape import chain
 import pytest
 
 AMOUNT = 10**21
-
+DAY = 86400
+WEEK = DAY * 7
+YEAR = DAY * 365
 
 @pytest.mark.parametrize("idx", range(5))
 def test_initial_state(accounts, veboost, ve, idx):
@@ -69,3 +72,41 @@ def test_boost_fails_with_invalid_amount_greater_than_delegable_balance(
 def test_boost_fails_with_invalid_allowance(alice, bob, veboost, lock_unlock_time):
     with ape.reverts():
         veboost.boost(bob, AMOUNT, lock_unlock_time, alice, sender=bob)
+
+def test_adjusted_balance_of_write(alice, bob, veboost, ve, lock_unlock_time):
+    balance = ve.balanceOf(alice)
+    amount = balance / 4
+    
+    # Delegate to bob
+    veboost.boost(bob, int(amount), lock_unlock_time, sender=alice)
+
+    delegated_point = veboost.delegated(alice)
+    received_point = veboost.received(alice)
+    
+    for i in range(10):
+        tx = veboost.adjusted_balance_of_write(alice, sender=bob)
+        delegable = veboost.delegable_balance(alice)
+        delegated = veboost.delegated_balance(alice)
+        ve_balance = ve.balanceOf(alice)
+        
+        # Verify new point has been written (checkpoint)
+        new_point = veboost.delegated(alice)
+        assert new_point.ts > delegated_point.ts
+        delegated_point = new_point
+
+        new_point = veboost.received(alice)
+        assert new_point.ts > received_point.ts
+        received_point = new_point
+        
+        # Invariant: VE balance is always the sum of delegable + delegated
+        assert ve_balance == delegable + delegated
+        assert veboost.balanceOf(alice) == tx.return_value == delegable
+
+        # Ensure bob's adjusted balance is equal to what Alice has delegated
+        tx = veboost.adjusted_balance_of_write(bob, sender=bob)
+        assert tx.return_value == veboost.balanceOf(bob)
+        assert veboost.delegated_balance(alice) == veboost.balanceOf(bob)
+        assert veboost.delegable_balance(bob) == 0 # Bob doesn't have any VE balance
+
+        chain.pending_timestamp += DAY * i
+        chain.mine()
